0ec0796fe3885b1de86a62237bbdad83e7e1aac7
Juan Morales <juanmorales@MacBook-Pro-de-Juan.local>
2025-08-25 19:51:25 -0500

    Se ajustan excepction handler

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/challenge/api/GlobalExceptionHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/challenge/api/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package co.com.pragma.challenge.api;
 
 import co.com.pragma.challenge.model.exception.EmailExistException;
+import co.com.pragma.challenge.model.exception.EmptyFieldException;
+import co.com.pragma.challenge.model.exception.InvalidValueException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,6 +20,37 @@ public class GlobalExceptionHandler {
                         "message", ex.getMessage()
                 )
         );
+    }
+
+    @ExceptionHandler(EmptyFieldException.class)
+    public ResponseEntity<Map<String, Object>> handleEmptyFieldException(EmptyFieldException ex) {
+        return ResponseEntity.badRequest().body(
+                Map.of(
+                        "code", ex.getCode(),
+                        "message", ex.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(EmailExistException.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(EmailExistException ex) {
+        return ResponseEntity.status(400).body(
+                Map.of(
+                        "code", ex.getCode(),
+                        "message", ex.getMessage()
+                )
+        );
+    }
+
+        @ExceptionHandler(InvalidValueException.class)
+        public ResponseEntity<Map<String, Object>> handleGenericException(InvalidValueException ex) {
+            return ResponseEntity.status(400).body(
+                    Map.of(
+                            "code", ex.getCode(),
+                            "message", ex.getMessage()
+                    )
+            );
+        }
     }
 
 
